c4bc52e58894b05c5dc5f1473fbd4c81be61b692
caohui <firstcaohui@qq.com>
2017-08-02 13:40:03 +0800

    composer init composer install head git

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # test
 test composer go
+composer require caohui/test
+
+```apple js
+$test = new \Lib\Test\test();
+echo $test->go();
+```
---
+++ src/Lib/Test/test.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2017/8/2
+ * Time: 11:00
+ */
+
+namespace Lib\test;
+
+class test
+{
+    public function go(){
+        return 'test';
+    }
+}
